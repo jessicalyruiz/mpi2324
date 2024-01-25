5f71349f6eb819f3ceeab45f8af001328ace597a
Jessicaly Ruiz <jessicalyr@gmail.com>
2024-01-25 04:53:25 +0000

    ejemplo1

--- ejemplo01MPI.cpp
+++ ejemplo01MPI.cpp
@@ -2,26 +2,15 @@
 // Created by gitpod on 12/19/23.
 //
 
-#include "ejemplo01MPI.h"
+#include <iostream>
 #include <mpi.h>
-int main(int argc, char** argv){
 
-    MPI_Init(&argc, &argv);
-
-    int rank, nprocs;
-
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank );
-
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-        std::println("rank %d ")
-
-
-
-
+int main(int argc,char** argv){
+    MPI_Init(&argc,&argv);
+    int rank,nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+    std::printf("Rank %d of %d procs\n",rank,nprocs);
     MPI_Finalize();
-
-
-
-    Return 0;
-
+    return 0;
 }
